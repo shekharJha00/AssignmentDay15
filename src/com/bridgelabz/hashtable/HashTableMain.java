905dec78190ef754ca8e7ce000efab1ac92f3d22
shekhar <shekharjha7417870578@gmail.com>
2022-08-23 06:47:54 +0530

    Added uc1 : count frequency in a sentence

---
+++ src/com/bridgelabz/hashtable/HashTableMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.hashtable;
+
+public class HashTableMain {
+    public static void main(String[] args) {
+
+
+
+                String str = "To be or not to be";
+                HashTable hashTable = new HashTable();
+                hashTable.countWords(str);
+            }
+
+        }
